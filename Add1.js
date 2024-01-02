e9bb05cfd0c39cbb46bf08b1df6dddb41815373d
Chocotapi <shioriavino@gmail.com>
2024-01-02 16:55:06 -0500

    O(n2) example

--- Add1.js
+++ Add1.js
@@ -1,3 +1,4 @@
+//O(n)
 function addUpTo(n) {
     let total = 0;
     for (let i = 1; i <= n; i++) {
@@ -6,4 +7,14 @@ function addUpTo(n) {
     return total;
 }
 
-console.log(addUpTo(5));
+//O(1)
+// function addUpTo(n) {
+//     return n * (n + 1) / 2
+// }
+
+var time1 = performance.now();
+addUpTo(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${time2 - time1 / 1000} seconds`)
+
+// console.log(addUpTo(5));
