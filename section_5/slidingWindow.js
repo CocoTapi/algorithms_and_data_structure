56f3cf7cdd356bf5c0cfba20387bedb574908e8a
Chocotapi <shioriavino@gmail.com>
2024-01-04 00:42:36 -0500

    added an example using sliding window

--- section_5/slidingWindow.js
+++ section_5/slidingWindow.js
@@ -8,4 +8,25 @@
 // maxSubarraySum([], 4) => null
 
 //step
-//
+// when you move, subtract the first number and add the last number
+
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) {
+        return null;
+    }
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum
+    for (let i = num; i<arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4));
+
+//time complexity O(n)
