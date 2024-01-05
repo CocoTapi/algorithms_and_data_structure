ef7500946139686bc924e9094e05c11a6bffff99
Chocotapi <shioriavino@gmail.com>
2024-01-05 17:54:23 -0500

    added searching example

--- section_9_recursion/capitalizeWords.js
+++ section_9_recursion/capitalizeWords.js
@@ -5,12 +5,14 @@
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
 
 function capitalizeWords (array) {
-    if (array.length === 1) {
-      return [array[0][0].toUpperCase() + array[0].substr(1)];
-    }
-    const res = capitalizeWords(array.slice(0, -1));
-    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
-    res.push(string);
-    return res;
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
   }
-  
+  const res = capitalizeWords(array.slice(0, -1));
+  const string = array.slice(array.length - 1)[0].toUpperCase();
+  res.push(string);
+  return res;
+}
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
