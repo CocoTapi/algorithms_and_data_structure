ce0151ccc6eed92537af66e9918ad267f9f520a9
Chocotapi <shioriavino@gmail.com>
2024-01-05 16:32:30 -0500

    added recursion examples

---
+++ section_9_recursion/capitalizeWords.js
@@ -0,0 +1,16 @@
+//Write a recursive function called capitalizeWords.
+//Given an array of words, return a new array containing each word capitalized.
+
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+function capitalizeWords (array) {
+    if (array.length === 1) {
+      return [array[0][0].toUpperCase() + array[0].substr(1)];
+    }
+    const res = capitalizeWords(array.slice(0, -1));
+    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+    res.push(string);
+    return res;
+  }
+  
