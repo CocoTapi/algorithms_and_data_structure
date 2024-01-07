ac18ac2bf1df4d3af04b139ca81d786778a2531d
Chocotapi <shioriavino@gmail.com>
2024-01-07 16:53:33 -0500

    added bubble Sort

---
+++ section_11/bubbleSort.js
@@ -0,0 +1,32 @@
+// function babbleSort(arr) {
+//     for (let i = arr.length; i > 0; i--){
+//         for (let j = 0; j < i - 1; j++){
+//             console.log(arr, arr[j], arr[j + 1]);
+//             if (arr[j] > arr[j + 1]){
+//                 //swap
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+function bubbleSort(arr){
+    const swap = (arr, index1, index2) => {
+        [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
+    };
+
+    for (let i = arr.length; i > 0; i--){
+        for (let j = 0; j < i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+const demo = bubbleSort([3, 37, 22, 45, 29, 8]);
+console.log(demo);
