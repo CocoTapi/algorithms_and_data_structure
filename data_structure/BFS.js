8a5a13ee2a15871d5f61737e8113578f609f9789
Chocotapi <shioriavino@gmail.com>
2024-01-09 17:19:56 -0500

    added BFS

---
+++ data_structure/BFS.js
@@ -0,0 +1,101 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while (true) {
+                if (value === current.value) return undefined;
+                if (value < current.value) {
+                    if ( current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } 
+                        current = current.left;
+                } 
+                if ( current.right === null) {
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right; 
+            }
+        }
+    }
+    find(value) {
+        if (this.root === null) return false;
+        var current = this.root;
+        var found = false;
+        while(current && !found){
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
+    contains(value) {
+        if (this.root === null) return false;
+        var current = this.root;
+        var found = false;
+        while(current && !found){
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        return false;
+    }
+    BFS() {
+        var node = this.root,
+            data = [],
+            queue = [];
+    
+            queue.push(node);
+
+            while(queue.length) {
+                node = queue.shift();
+                data.push(node.value);
+                if (node.left) queue.push(node.left);
+                if (node.right) queue.push(node.right)
+            }
+        return data;
+    }
+
+}
+
+//       10
+//    5        13
+// 2    7   11     
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(2);
+tree.insert(7);
+tree.insert(11);
+
+console.log(tree.BFS());
+
+
+
