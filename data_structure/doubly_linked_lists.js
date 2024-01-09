fda5668f252fe7599474b8fae2c1696226d51b78
Chocotapi <shioriavino@gmail.com>
2024-01-09 13:08:39 -0500

    added pop and shift

--- data_structure/doubly_linked_lists.js
+++ data_structure/doubly_linked_lists.js
@@ -25,10 +25,41 @@ class DoublyLinkedList{
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.head) return undefined;
+        var poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
+    shift(){
+        if (this.length === 0) return undefined;
+        var oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
 }
 
 var list = new DoublyLinkedList();
+console.log(list.push(98));
+console.log(list.push(99));
 console.log(list.push(100));
 console.log(list.push(101));
+console.log(list.shift());
 
 
