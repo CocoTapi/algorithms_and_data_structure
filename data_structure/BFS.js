7e33bbd6d73670b9cfb43a2346929354bf9b9cad
Chocotapi <shioriavino@gmail.com>
2024-01-09 22:46:16 -0500

    fixed insert and added DFS Post-Order

--- data_structure/BFS.js
+++ data_structure/BFS.js
@@ -10,28 +10,55 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+    // insert(value) {
+    //     var newNode = new Node(value);
+    //     if (this.root === null) {
+    //         this.root = newNode;
+    //         return this;
+    //     } else {
+    //         var current = this.root;
+    //         while (true) {
+    //             if (value === current.value) return undefined;
+    //             if (value < current.value) {
+    //                 if (current.left === null) {
+    //                     current.left = newNode;
+    //                     return this;
+    //                 } else {
+    //                     current = current.left;
+    //                 }
+    //             } else if (value > current.value) {
+    //                 if ( current.right === null) {
+    //                     current.right = newNode;
+    //                     return this;
+    //                 } else {
+    //                     current = current.right; 
+    //                 }
+    //             }
+    //         }
+    //     }    
+    // }
     insert(value) {
         var newNode = new Node(value);
         if (this.root === null) {
             this.root = newNode;
             return this;
-        } else {
-            var current = this.root;
-            while (true) {
-                if (value === current.value) return undefined;
-                if (value < current.value) {
-                    if ( current.left === null) {
-                        current.left = newNode;
-                        return this;
-                    } 
-                        current = current.left;
-                } 
-                if ( current.right === null) {
+        }
+        var current = this.root;
+        while(true) {
+            if (value === current/value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left
+            } else {
+                if (current.right === null) {
                     current.right = newNode;
                     return this;
-                } 
-                current = current.right; 
-            }
+                }
+                current = current.right
+            }          
         }
     }
     find(value) {
@@ -80,22 +107,42 @@ class BinarySearchTree {
             }
         return data;
     }
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
 
 }
 
 //       10
-//    5        13
-// 2    7   11     
+//    6        15
+// 3    8          20    
 
 var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(2);
-tree.insert(7);
-tree.insert(11);
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree.BFS());
+console.log(tree.DFSPostOrder());
 
 
 
