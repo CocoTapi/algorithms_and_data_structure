45014a7c2244c948890086020242a4c68ecc1141
Chocotapi <shioriavino@gmail.com>
2024-01-11 13:04:58 -0500

    added remove vertex

--- data_structure/graph.js
+++ data_structure/graph.js
@@ -17,13 +17,25 @@ class Graph {
             v => v !== vertex1
         );
     }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
 }
 
 let g = new Graph();
 g.addVertex("Tokyo");
 g.addVertex("Paris");
 g.addVertex("Dallas");
+g.addVertex("Hong Kong")
 g.addEdge("Dallas", "Tokyo");
 g.addEdge("Paris", "Tokyo");
-g.removeEdge("Tokyo", "Dallas");
+g.addEdge("Dallas", "Hong Kong");
+g.addEdge("Tokyo", "Hong Kong");
+g.addEdge("Paris", "Hong Kong");
+g.addEdge("Paris", "Dallas");
+g.removeVertex("Hong Kong");
 console.log(g);
