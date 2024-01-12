4acabe997700a8312759a0b567cb75231b5d371b
Chocotapi <shioriavino@gmail.com>
2024-01-11 20:03:55 -0500

    added a faster priority que

--- data_structure/dijkstra_pq.js
+++ data_structure/dijkstra_pq.js
@@ -1,20 +1,3 @@
-class PriorityQueue {
-    constructor() {
-        this.values = [];
-    }
-    //naive way O(n log(n))
-    enqueue(val, priority) {
-        this.values.push({val, priority});
-        this.sort();
-    };
-    dequeue() {
-        return this.values.shift();
-    };
-    sort() {
-        this.values.sort((a, b) => a.priority - b.priority);
-    }
-}
-
 class weightedGraph {
     constructor() {
         this.adjacencyList = {};
@@ -83,7 +66,77 @@ class weightedGraph {
 
 }
 
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority)
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+            if (element.priority >= parent.priority) break;
+            //swap
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            //update index
+            idx = parentIdx
+        }
+    }
+    dequeue() {
+        const min = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
 
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority) {
+                    swap = leftChildIdx
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                    swap = rightChildIdx
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+}
+
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
 
 let graph = new weightedGraph();
 graph.addVertex("A");
@@ -94,7 +147,7 @@ graph.addVertex("E");
 graph.addVertex("F");
 
 graph.addEdge("A", "B", 4);
-graph.addEdge("A", "C", 3);
+graph.addEdge("A", "C", 2);
 graph.addEdge("B", "E", 3);
 graph.addEdge("C", "D", 2);
 graph.addEdge("C", "F", 4);
@@ -103,6 +156,3 @@ graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
 
 console.log(graph.Dijkstra("A", "E"));
-
-
-
