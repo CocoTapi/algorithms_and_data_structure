7f1b04a62633289af83a592631fd0b19a44ee577
Chocotapi <shioriavino@gmail.com>
2024-02-01 22:58:39 -0500

    added another answer

--- recursion/productOfArray.js
+++ recursion/productOfArray.js
@@ -10,3 +10,9 @@ function productOfArray(arr) {
   return total;
 }
 
+//2
+function productOfArray(arr) {
+    if (arr.length === 0) return 1;
+    
+    return arr[0] * productOfArray(arr.slice(1));
+ }
