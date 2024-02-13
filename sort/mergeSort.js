14df638dcbca9c9c88f53c89f45ea4b7adef3c39
Chocotapi <shioriavino@gmail.com>
2024-02-13 17:30:39 -0500

    added merge function

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -1,36 +1,64 @@
-function merge(arr1, arr2) {
+// function merge(arr1, arr2) {
+//     let results = [];
+//     let i = 0;
+//     let j = 0;
+//     while (i < arr1.length && j < arr2.length){
+//         if (arr2[j] > arr1[i]){
+//             results.push(arr1[i]);
+//             i++
+//         } else {
+//             results.push(arr2[j]);
+//             j++
+//         }
+//     }
+//     while( i < arr1.length) {
+//         results.push(arr1[i]);
+//         i++
+//     }
+//     while( j < arr2.length) {
+//         results.push(arr2[j]);
+//         j++
+//     }
+//     return results;
+// }
+
+// function mergeSort(arr) {
+//     if (arr.length <= 1) return arr;
+    
+//     let mid = Math.floor(arr.length / 2);
+//     let left = mergeSort(arr.slice(0, mid));
+//     let right = mergeSort(arr.slice(mid));
+//     return merge(left, right)
+
+// } 
+
+var merge = function(nums1, m, nums2, n) {
     let results = [];
     let i = 0;
     let j = 0;
-    while (i < arr1.length && j < arr2.length){
-        if (arr2[j] > arr1[i]){
-            results.push(arr1[i]);
-            i++
+
+    while (i < m && j < n) {
+        if (nums1[i] < nums2[j]) {
+            results.push(nums1[i]);
+            i++;
         } else {
-            results.push(arr2[j]);
-            j++
+            results.push(nums2[j]);
+            j++;
         }
+    } 
+
+    while (i < m) {
+        results.push(nums1[i]);
+        i++;
     }
-    while( i < arr1.length) {
-        results.push(arr1[i]);
-        i++
+
+    while (j < n) {
+        results.push(nums2[j]);
+        j++;
     }
-    while( j < arr2.length) {
-        results.push(arr2[j]);
-        j++
-    }
+
     return results;
-}
+};
 
-function mergeSort(arr) {
-    if (arr.length <= 1) return arr;
-    
-    let mid = Math.floor(arr.length / 2);
-    let left = mergeSort(arr.slice(0, mid));
-    let right = mergeSort(arr.slice(mid));
-    return merge(left, right)
-
-} 
-
-const demo = mergeSort([1, 10, 50, 2, 14, 99, 100]);
+const demo = merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
 console.log(demo);
